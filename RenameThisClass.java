da05420349d71c0ddbd8415e3edd16371f677a62
Derek Ngo <derek.ngo@mrjaffesclass.com>
2022-09-16 14:16:38 -0700

    1

--- RenameThisClass.java
+++ RenameThisClass.java
@@ -1,13 +1,16 @@
-
-/**
- * Write a description of class RenameThisClass here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
 public class RenameThisClass
 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        int i;
+        double f = 2.5;
+        boolean flag = true;
+        i = 10;
+        f = i + f;
+        flag = !flag;
+        ++i;
+        System.out.print("i");
+        System.out.print("f");
+        System.out.print("flag");
     }
 }
